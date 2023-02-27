cec913d8f9d4d9ca7769e6d5be1a42d421664b1b
shenlindao <shenlin@yandex.ru>
2023-02-27 12:35:07 +0500

    Ex004_new_fix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Square(int A, int B) {
+    if (A == B * B) Console.WriteLine("А является квадратом B");
+    else Console.WriteLine("А не является квадратом B");
+
+    if (B == A * A) Console.WriteLine("B является квадратом A");
+    else Console.WriteLine("B не является квадратом A");
+}
+
+Console.Write("Число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Square(A, B);
